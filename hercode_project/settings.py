d3ee2e4dfc20fe6908470b34dea3eb0d443d6ec2
sonia250 <s.uwase3@alustudent.com>
2025-11-14 15:04:24 +0200

    Update ALLOWED_HOSTS for deployment

--- hercode_project/settings.py
+++ hercode_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-uu526xkh(r%nabbzjef40brw(gb2m*662%gswv!s_&t_ql5)-o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sonia250.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
